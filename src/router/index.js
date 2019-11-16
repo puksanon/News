c07492df7aff505eed5ab2079f76424514f93e0b
puksanon <kittipit22@hotmail.com>
2019-11-17 02:32:23 +0700

    up

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 Vue.use(VueRouter);
-const Index  = () => import('../components/index/index')
+// const Index  = () => import('../components/index/index')
 
 //main page
 const home   = () => import('@/components/home/home')
