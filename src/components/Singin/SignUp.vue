08d0e125e0d5bd71e62b46d0024729a6aeb00e8f
MuchaluchaZ <kittipit22@hotmail.com>
2020-02-18 08:15:04 +0700

    update admin view

--- src/components/Singin/SignUp.vue
+++ src/components/Singin/SignUp.vue
@@ -2,9 +2,20 @@
   <v-row style="padding-left: 25px;">
     <v-dialog v-model="dialog" fullscreen transition="dialog-bottom-transition">
       <template v-slot:activator="{ on }">
-        <v-btn color="primary" dark v-on="on">Sign UP</v-btn>
+        <v-btn color="primary" dark v-on="on">Sign Up</v-btn>
       </template>
-      <v-card outlined>
+      <v-card>
+          <v-toolbar dark color="primary">
+          <v-btn icon dark @click="dialog = false">
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+          <v-toolbar-title>SIGN UP</v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-toolbar-items>
+            <v-btn dark text @click="dialog = false">Sign Up</v-btn>
+          </v-toolbar-items>
+        </v-toolbar>
+    
             <v-list three-line subheader>
                 <v-subheader>User Controls</v-subheader>
                 <v-list-item>
@@ -63,79 +74,80 @@
                     </v-row>
                 </v-list-item>
             </v-list>
-                <v-divider></v-divider>
-                <v-list three-line subheader>
-                <v-subheader>Select favorite types(3) </v-subheader>
-                    <v-row>
-                        <v-col cols="12" sm="6" md="4">
-                            <v-list-item>
-                                <v-list-item-action>
-                                <v-checkbox v-model="q1"></v-checkbox>
-                                </v-list-item-action>
-                                <v-list-item-content>
-                                <v-list-item-title>SPORT</v-list-item-title>
-                                <v-list-item-subtitle>Notify me about updates to apps or games that I downloaded</v-list-item-subtitle>
-                                </v-list-item-content>
-                            </v-list-item>
-                        </v-col>
-                        <v-col cols="12" sm="6" md="4">
-                            <v-list-item>
-                                <v-list-item-action>
-                                <v-checkbox v-model="q2"></v-checkbox>
-                                </v-list-item-action>
-                                <v-list-item-content>
-                                <v-list-item-title>IT</v-list-item-title>
-                                <v-list-item-subtitle>Notify me about updates to apps or games that I downloaded</v-list-item-subtitle>
-                                </v-list-item-content>
-                            </v-list-item>
-                        </v-col>
-                        <v-col cols="12" sm="6" md="4">
-                            <v-list-item>
-                                <v-list-item-action>
-                                <v-checkbox v-model="q3"></v-checkbox>
-                                </v-list-item-action>
-                                <v-list-item-content>
-                                <v-list-item-title>GAME</v-list-item-title>
-                                <v-list-item-subtitle>Notify me about updates to apps or games that I downloaded</v-list-item-subtitle>
-                                </v-list-item-content>
-                            </v-list-item>
-                        </v-col>
-                        <v-col cols="12" sm="6" md="4">
-                            <v-list-item>
-                                <v-list-item-action>
-                                <v-checkbox v-model="q4"></v-checkbox>
-                                </v-list-item-action>
-                                <v-list-item-content> 
-                                <v-list-item-title>LIFE</v-list-item-title>
-                                <v-list-item-subtitle>Notify me about updates to apps or games that I downloaded</v-list-item-subtitle>
-                                </v-list-item-content>
-                            </v-list-item>
-                        </v-col>
-                        <v-col cols="12" sm="6" md="4">
-                            <v-list-item>
-                                <v-list-item-action>
-                                <v-checkbox v-model="q5"></v-checkbox>
-                                </v-list-item-action>
-                                <v-list-item-content>
-                                <v-list-item-title>POLICY</v-list-item-title>
-                                <v-list-item-subtitle>Notify me about updates to apps or games that I downloaded</v-list-item-subtitle>
-                                </v-list-item-content>
-                            </v-list-item>
-                        </v-col>
-                        <v-col cols="12" sm="6" md="4">
-                            <v-list-item>
-                                <v-list-item-action>
-                                <v-checkbox v-model="q6"></v-checkbox>
-                                </v-list-item-action>
-                                <v-list-item-content>
-                                <v-list-item-title>NATURE</v-list-item-title>
-                                <v-list-item-subtitle>Notify me about updates to apps or games that I downloaded</v-list-item-subtitle>
-                                </v-list-item-content>
-                            </v-list-item>
-                        </v-col>
-                    </v-row>
+            <v-divider></v-divider>
+            <v-list three-line subheader>
+            <v-subheader>Select favorite types(3)</v-subheader>
+                <v-row>
+                    <v-col cols="12" sm="6" md="4">
+                        <v-list-item>
+                            <v-list-item-action>
+                            <v-checkbox v-model="q1"></v-checkbox>
+                            </v-list-item-action>
+                            <v-list-item-content>
+                            <v-list-item-title>SPORT</v-list-item-title>
+                            <v-list-item-subtitle>Notify me about updates to apps or games that I downloaded</v-list-item-subtitle>
+                            </v-list-item-content>
+                        </v-list-item>
+                    </v-col>
+                    <v-col cols="12" sm="6" md="4">
+                        <v-list-item>
+                            <v-list-item-action>
+                            <v-checkbox v-model="q2"></v-checkbox>
+                            </v-list-item-action>
+                            <v-list-item-content>
+                            <v-list-item-title>IT</v-list-item-title>
+                            <v-list-item-subtitle>Notify me about updates to apps or games that I downloaded</v-list-item-subtitle>
+                            </v-list-item-content>
+                        </v-list-item>
+                    </v-col>
+                    <v-col cols="12" sm="6" md="4">
+                        <v-list-item>
+                            <v-list-item-action>
+                            <v-checkbox v-model="q3"></v-checkbox>
+                            </v-list-item-action>
+                            <v-list-item-content>
+                            <v-list-item-title>GAME</v-list-item-title>
+                            <v-list-item-subtitle>Notify me about updates to apps or games that I downloaded</v-list-item-subtitle>
+                            </v-list-item-content>
+                        </v-list-item>
+                    </v-col>
+                    <v-col cols="12" sm="6" md="4">
+                        <v-list-item>
+                            <v-list-item-action>
+                            <v-checkbox v-model="q4"></v-checkbox>
+                            </v-list-item-action>
+                            <v-list-item-content> 
+                            <v-list-item-title>LIFE</v-list-item-title>
+                            <v-list-item-subtitle>Notify me about updates to apps or games that I downloaded</v-list-item-subtitle>
+                            </v-list-item-content>
+                        </v-list-item>
+                    </v-col>
+                    <v-col cols="12" sm="6" md="4">
+                        <v-list-item>
+                            <v-list-item-action>
+                            <v-checkbox v-model="q5"></v-checkbox>
+                            </v-list-item-action>
+                            <v-list-item-content>
+                            <v-list-item-title>POLICY</v-list-item-title>
+                            <v-list-item-subtitle>Notify me about updates to apps or games that I downloaded</v-list-item-subtitle>
+                            </v-list-item-content>
+                        </v-list-item>
+                    </v-col>
+                    <v-col cols="12" sm="6" md="4">
+                        <v-list-item>
+                            <v-list-item-action>
+                            <v-checkbox v-model="q6"></v-checkbox>
+                            </v-list-item-action>
+                            <v-list-item-content>
+                            <v-list-item-title>NATURE</v-list-item-title>
+                            <v-list-item-subtitle>Notify me about updates to apps or games that I downloaded</v-list-item-subtitle>
+                            </v-list-item-content>
+                        </v-list-item>
+                    </v-col>
+                </v-row>
             </v-list>
-        </v-card>
+       
+      </v-card>
     </v-dialog>
   </v-row>
 </template>
