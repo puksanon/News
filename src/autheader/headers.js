f400b0173f4ae1e2bdd8de3be2a2723698210f50
untesler <keypgphysics@outlook.com>
2020-05-29 17:21:16 +0700

    Add Allow origin * into header

--- src/autheader/headers.js
+++ src/autheader/headers.js
@@ -1,6 +1,7 @@
 export default function authHeader () {
     return {
       'Authorization': "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoic3VtbWFyaXplU2VydmVyIiwiZHQiOiIyMDE2LTA4LTI5VDA5OjEyOjMzLjAwMVoiLCJpYXQiOjE1OTAwMTcxMjN9.5644l8oVxelZBIBUIajc-mPwMG0nqEVwZk9248FR74Y",
-      'Content-Type': 'application/x-www-form-urlencoded'
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Access-Control-Allow-Origin': '*',
     }
   }
